794e29b824346c5773b2f5237e73b4ba9c780975
srilaxmi-1992 <srilaxmilucky123@gmail.com>
2026-03-29 12:48:39 -0400

    added retry listener

--- src/test/java/listeners/RetryListener.java
+++ src/test/java/listeners/RetryListener.java
@@ -1,4 +1,17 @@
 package listeners;
 
-public class RetryListener {
+import org.testng.IAnnotationTransformer;
+import org.testng.annotations.ITestAnnotation;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+public class RetryListener implements IAnnotationTransformer {
+
+    @Override
+    public void transform(ITestAnnotation annotation, Class testClass,
+                          Constructor testConstructor, Method testMethod) {
+        // Applies RetryAnalyzer to ALL tests automatically — no need to add per test
+        annotation.setRetryAnalyzer(RetryAnalyzer.class);
+    }
 }
