fb269fd1f44c2e0fea315deb21a834e707db535e
srilaxmi-1992 <srilaxmilucky123@gmail.com>
2026-04-01 19:00:41 -0400

    updated jenkins pipeline config file

--- jenkins_pipeline.groovy
+++ jenkins_pipeline.groovy
@@ -16,7 +16,7 @@ pipeline {
         }
         stage('Build') {
             steps {
-                bat 'mvn clean install'
+                bat 'mvn clean install -DskipTests'
             }
         }
         stage('Test') {
