88933a09668b4e8131a623114d6b5b0c66e1abb9
srilaxmi-1992 <srilaxmilucky123@gmail.com>
2026-04-01 12:59:41 -0400

    graphql test cases added- missed file

--- src/test/java/utils/GraphQLUtils.java
+++ src/test/java/utils/GraphQLUtils.java
@@ -1,4 +1,16 @@
 package utils;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
 public class GraphQLUtils {
+
+    public static Response executeQuery(String baseUri, String body) {
+        return RestAssured.given()
+                .log().all()
+                .baseUri(baseUri)
+                .contentType("application/json")
+                .body(body)
+                .post();
+    }
 }
